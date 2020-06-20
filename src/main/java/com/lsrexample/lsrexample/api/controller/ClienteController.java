157276f9b36790e755ab89d0c672d00ae06ef482
Rossitto <lucas74rossi@gmail.com>
2020-06-20 12:00:28 -0300

    first commit

---
+++ src/main/java/com/lsrexample/lsrexample/api/controller/ClienteController.java
@@ -0,0 +1,26 @@
+package com.lsrexample.lsrexample.api.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.lsrexample.lsrexample.domain.model.Cliente;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClienteController {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @GetMapping("/clientes")
+    public List<Cliente> listar() {
+        return manager.createQuery("from Cliente", Cliente.class).getResultList();
+
+    }
+
+}
---
+++ src/main/java/com/lsrexample/lsrexample/domain/model/Cliente.java
@@ -0,0 +1,73 @@
+package com.lsrexample.lsrexample.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "mail")
+    private String email;
+
+    @Column(name = "telefone")
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
