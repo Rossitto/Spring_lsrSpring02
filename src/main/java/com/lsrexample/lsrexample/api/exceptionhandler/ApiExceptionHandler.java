fd32093a128b0d39e577ce16f8706e1fbff330f7
Rossitto <lucas74rossi@gmail.com>
2020-06-20 16:51:00 -0300

    added messsages.properties

--- src/main/java/com/lsrexample/lsrexample/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/lsrexample/lsrexample/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,9 +3,13 @@ package com.lsrexample.lsrexample.api.exceptionhandler;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,6 +19,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Autowired
+    private MessageSource messageSource;
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
             HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -22,8 +29,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         var campos = new ArrayList<Problema.Campo>();
 
         for (ObjectError error : ex.getBindingResult().getAllErrors()) {
-            String nome = error.getObjectName();
-            String mensagem = error.getDefaultMessage();
+            String nome = ((FieldError) error).getField();
+            String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
 
             campos.add(new Problema.Campo(nome, mensagem));
         }
